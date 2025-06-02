8d2fec97f61ac4d7c3df7fd8772cf647b78bcafc
Badhan Saha <badhansaha@yahoo.com>
2025-06-02 20:22:45 +0600

    update

--- provider.tf
+++ provider.tf
@@ -2,13 +2,13 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "5.97.0"
+      version = "5.99.1"
 
     }
   }
 }
 
 provider "aws" {
-  region  = "ap-southeast-1"
-  profile = "badhansaha"
+  region  = "us-west-2"
+  profile = "yourmentors"
 }
